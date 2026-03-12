d6bff8618e7c0627fdc23b4270c23d263192d3ef
Arjun Shah <arjunsah856@gmail.com>
2026-03-12 18:45:38 +0545

    db connection code db.js

---
+++ database/db.js
@@ -0,0 +1,13 @@
+// postgres db connection code using drizzle-orm
+
+import postgres from "postgres"     //mports the postgres client library.
+import { drizzle } from "drizzle-orm"   //imports the Drizzle ORM driver used to connect database client to Drizzle.
+
+
+const connStr = process.env.DATABASE_URL
+
+const connection = postgres(connStr)     // creates a PostgreSQL connection instance using the connection string.
+
+const db = dr(connection) //Initialize Drizzle ORM,,, Now instead of writing raw SQL everywhere, we can use Drizzle's typed query system.
+
+export default db
