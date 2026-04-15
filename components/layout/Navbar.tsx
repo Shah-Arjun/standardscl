f1e510b7b257a9db39183407e62466936a8d849d
Arjun Shah <arjunsah856@gmail.com>
2026-04-15 10:32:50 +0545

    navbar

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -69,10 +69,10 @@ export const Navbar = () => {
     >
       <div className="w-full mx-auto px-4 md:px-8">
         {/* desktop navbar */}
-        <div className="flex h-16 lg:h-18 items-center justify-between">
+        <div className="flex h-18 items-center justify-between">
           {/* logo */}
-          <Link href="/" className="flex items-center gap-3 ">
-            <Image src={SchoolLogo} width={72} height={72} priority className="h-16 w-16 lg:h-18 lg:w-18 object-contain" alt="logo"/>
+          <Link href="/" className="flex items-center gap-3">
+            <Image src={SchoolLogo} width={72} height={72} priority className="object-contain" alt="logo"/>
             {/* logo-text */}
             <div className="hidden md:block leading-tight">
               <h1 className="font-bold text-2xl tracking-tight">Standard Secondary</h1>
@@ -131,7 +131,7 @@ export const Navbar = () => {
                     label: "Navbar",
                   })
                 }
-                className="inline-flex items-center justify-center px-2 py-2 text-md lg:text-lg font-medium rounded-4xl btn-primary-school text-white hover:border-2 border-secondary"
+                className="inline-flex items-center justify-center px-2 py-2.5 text-sm md:text-md lg:text-lg font-medium rounded-4xl btn-primary-school text-white hover:border-2 border-secondary"
               >
                 Get Admission
               </Link>
@@ -141,10 +141,10 @@ export const Navbar = () => {
             <button
               type="button"
               onClick={() => setIsOpen(!isOpen)}
-              className="lg:hidden ml-2 rounded-md  transition-colors"
+              className="lg:hidden ml-2 transition-colors"
               aria-label="Toggle menu"
             >
-              {isOpen ? "" : <Menu size={22} />}
+              {isOpen ? <X size={24} /> : <Menu size={24} />}
             </button>
           </div>
         </div>
